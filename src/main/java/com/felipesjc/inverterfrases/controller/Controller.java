cda508175993d0c74118925eb412d28c397dc90b
Felipesjc17 <felipe_sjc17@hotmail.com>
2022-03-24 21:39:37 -0300

    Cors

--- src/main/java/com/felipesjc/inverterfrases/controller/Controller.java
+++ src/main/java/com/felipesjc/inverterfrases/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.felipesjc.inverterfrases.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.felipesjc.inverterfrases.main.Frase;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/frase")
 public class Controller {
